4f8552e002d9b1e679b5eaaa773dd35d521305ee
Andrew Golovnev <andrey.golovnev71@yandex.ru>
2023-03-30 23:27:27 +0300

    Setup basic Application and Entry Point.

---
+++ Sandbox/src/SandboxApp.cpp
@@ -0,0 +1,21 @@
+#include <Sirius.h>
+
+class Sandbox : public Sirius::Application
+{
+public:
+	Sandbox()
+	{
+
+	}
+
+	~Sandbox()
+	{
+
+	}
+
+};
+
+Sirius::Application* Sirius::CreateApplication()
+{
+	return new Sandbox();
+}
---
+++ Sirius/src/Sirius/Core.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef SRS_PLATFORM_WINDOWS
+	#ifdef SRS_BUILD_DLL
+		#define SIRIUS_API __declspec(dllexport)
+	#else
+		#define SIRIUS_API __declspec(dllimport)
+	#endif
+#else
+	#error Sirius only supports Windows!
+#endif
