aa68e8faef6021dabaa47167d21968a59babbade
Andrew Golovnev <andrey.golovnev71@yandex.ru>
2023-04-01 05:47:45 +0300

    Added events to ImGui layer.

--- Sirius/src/Sirius/ImGui/ImGuiLayer.h
+++ Sirius/src/Sirius/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Sirius/Layer.h"
 
+#include "Sirius/Events/ApplicationEvent.h"
+#include "Sirius/Events/KeyEvent.h"
+#include "Sirius/Events/MouseEvent.h"
+
 namespace Sirius {
 
 	class SIRIUS_API ImGuiLayer : public Layer
@@ -14,6 +18,15 @@ namespace Sirius {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
 	private:
 		float m_Time = 0.0f;
 	};
