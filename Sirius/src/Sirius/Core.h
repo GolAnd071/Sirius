5772ba6f7aa74373a4eb41ed421d25030b4e76b3
Andrew Golovnev <andrey.golovnev71@yandex.ru>
2023-04-02 18:51:26 +0300

    Added Sirius::Scope and Sirius::Ref definitions

--- Sirius/src/Sirius/Core.h
+++ Sirius/src/Sirius/Core.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <memory>
+
 #ifdef SRS_PLATFORM_WINDOWS
 #if SRS_DYNAMIC_LINK
 	#ifdef SRS_BUILD_DLL
@@ -29,3 +31,13 @@
 #define BIT(x) (1 << x)
 
 #define SRS_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+
+namespace Sirius {
+
+	template<typename T>
+	using Scope = std::unique_ptr<T>;
+
+	template<typename T>
+	using Ref = std::shared_ptr<T>;
+
+}
