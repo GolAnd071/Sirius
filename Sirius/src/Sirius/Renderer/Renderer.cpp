26d34a33499bc474c2ffbe3e99c3078a2a7c235a
Andrew Golovnev <andrey.golovnev71@yandex.ru>
2023-04-02 18:46:35 +0300

    Moved OpenGL shader implementation to OpenGLShader class + added more uniform functions.

--- Sirius/src/Sirius/Renderer/Renderer.cpp
+++ Sirius/src/Sirius/Renderer/Renderer.cpp
@@ -1,6 +1,8 @@
 #include "srspch.h"
 #include "Renderer.h"
 
+#include "Platform/OpenGL/OpenGLShader.h"
+
 namespace Sirius {
 
 	Renderer::SceneData* Renderer::s_SceneData = new Renderer::SceneData;
@@ -17,8 +19,8 @@ namespace Sirius {
 	void Renderer::Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray, const glm::mat4& transform)
 	{
 		shader->Bind();
-		shader->UploadUniformMat4("u_ViewProjection", s_SceneData->ViewProjectionMatrix);
-		shader->UploadUniformMat4("u_Transform", transform);
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_ViewProjection", s_SceneData->ViewProjectionMatrix);
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_Transform", transform);
 
 		vertexArray->Bind();
 		RenderCommand::DrawIndexed(vertexArray);
