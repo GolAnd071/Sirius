fb15c342405434bb95d31791e0cdfa0f17a30e17
Andrew Golovnev <andrey.golovnev71@yandex.ru>
2023-04-03 20:42:01 +0300

    Shaders can now be read from files.

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -128,40 +128,7 @@ public:
 
 		m_FlatColorShader.reset(Sirius::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 
-		std::string textureShaderVertexSrc = R"(
-			#version 330 core
-			
-			layout(location = 0) in vec3 a_Position;
-			layout(location = 1) in vec2 a_TexCoord;
-
-			uniform mat4 u_ViewProjection;
-			uniform mat4 u_Transform;
-
-			out vec2 v_TexCoord;
-
-			void main()
-			{
-				v_TexCoord = a_TexCoord;
-				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
-			}
-		)";
-
-		std::string textureShaderFragmentSrc = R"(
-			#version 330 core
-			
-			layout(location = 0) out vec4 color;
-
-			in vec2 v_TexCoord;
-			
-			uniform sampler2D u_Texture;
-
-			void main()
-			{
-				color = texture(u_Texture, v_TexCoord);
-			}
-		)";
-
-		m_TextureShader.reset(Sirius::Shader::Create(textureShaderVertexSrc, textureShaderFragmentSrc));
+		m_TextureShader.reset(Sirius::Shader::Create("assets/shaders/Texture.glsl"));
 
 		m_Texture = Sirius::Texture2D::Create("assets/textures/Checkerboard.png");
 		m_GolAnd071LogoTexture = Sirius::Texture2D::Create("assets/textures/GolAnd071Logo.png");
