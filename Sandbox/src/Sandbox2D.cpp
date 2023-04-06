75176edd015b007ef279a49b8cf18b89053b0bd1
Andrew Golovnev <andrey.golovnev71@yandex.ru>
2023-04-06 10:28:46 +0300

    Updated Sandbox2D

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -38,9 +38,9 @@ void Sandbox2D::OnUpdate(Sirius::Timestep ts)
 	{
 		SRS_PROFILE_SCOPE("Renderer Draw");
 		Sirius::Renderer2D::BeginScene(m_CameraController.GetCamera());
-		Sirius::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+		Sirius::Renderer2D::DrawRotatedQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, glm::radians(-45.0f), {0.8f, 0.2f, 0.3f, 1.0f});
 		Sirius::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
-		Sirius::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture);
+		Sirius::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
 		Sirius::Renderer2D::EndScene();
 	}
 }
