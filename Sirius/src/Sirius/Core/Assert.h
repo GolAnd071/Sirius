7a29df92d92b9b2b9b76b27ccd80a927975d87a4
Andrew Golovnev <andrey.golovnev71@yandex.ru>
2023-04-07 07:30:47 +0300

    Multiple small improvements

--- Sirius/src/Sirius/Core/Assert.h
+++ Sirius/src/Sirius/Core/Assert.h
@@ -2,30 +2,15 @@
 
 #include "Sirius/Core/Base.h"
 #include "Sirius/Core/Log.h"
+#include <filesystem>
 
 #ifdef SRS_ENABLE_ASSERTS
-
-	namespace Sirius::Assert
-	{
-		// Returns the simple file name rather than full path
-		constexpr const char* CurrentFileName(const char* path) {
-			const char* file = path;
-			while (*path)
-			{
-				if (*path == '/' || *path == '\\')
-					file = ++path;
-				else
-					path++;
-			}
-			return file;
-		}
-	}
 
 	// Alteratively we could use the same "default" message for both "WITH_MSG" and "NO_MSG" and
 	// provide support for custom formatting by concatenating the formatting string instead of having the format inside the default message
 	#define SRS_INTERNAL_ASSERT_IMPL(type, check, msg, ...) { if(!(check)) { SRS##type##ERROR(msg, __VA_ARGS__); SRS_DEBUGBREAK(); } }
 	#define SRS_INTERNAL_ASSERT_WITH_MSG(type, check, ...) SRS_INTERNAL_ASSERT_IMPL(type, check, "Assertion failed: {0}", __VA_ARGS__)
-	#define SRS_INTERNAL_ASSERT_NO_MSG(type, check) SRS_INTERNAL_ASSERT_IMPL(type, check, "Assertion '{0}' failed at {1}:{2}", SRS_STRINGIFY_MACRO(check), ::Sirius::Assert::CurrentFileName(__FILE__), __LINE__)
+	#define SRS_INTERNAL_ASSERT_NO_MSG(type, check) SRS_INTERNAL_ASSERT_IMPL(type, check, "Assertion '{0}' failed at {1}:{2}", SRS_STRINGIFY_MACRO(check), std::filesystem::path(__FILE__).filename().string(), __LINE__)
 
 	#define SRS_INTERNAL_ASSERT_GET_MACRO_NAME(arg1, arg2, macro, ...) macro
 	#define SRS_INTERNAL_ASSERT_GET_MACRO(...) SRS_EXPAND_MACRO( SRS_INTERNAL_ASSERT_GET_MACRO_NAME(__VA_ARGS__, SRS_INTERNAL_ASSERT_WITH_MSG, SRS_INTERNAL_ASSERT_NO_MSG) )
--- Sirius/src/Sirius/Renderer/Buffer.h
+++ Sirius/src/Sirius/Renderer/Buffer.h
@@ -70,7 +70,7 @@ namespace Sirius {
 	public:
 		BufferLayout() {}
 
-		BufferLayout(const std::initializer_list<BufferElement>& elements)
+		BufferLayout(std::initializer_list<BufferElement> elements)
 			: m_Elements(elements)
 		{
 			CalculateOffsetsAndStride();
